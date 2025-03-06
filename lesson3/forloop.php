6fd1bbd8ade92a7a198a5accde0f3e3dba54887f
robert123321-web <roberthaxhiu2012@gmail.com>
2025-03-06 20:44:43 +0100

    day3

---
+++ lesson3/forloop.php
@@ -0,0 +1,5 @@
+<?php
+
+for($x=0; $x<10; $x++)
+    echo "the number is $x <br>"
+?>
---
+++ lesson3/if.php
@@ -0,0 +1,11 @@
+<?php
+
+$age = 12;
+
+if($age > 10){
+    echo "numri eshte me i madh se 10";
+}else{
+    echo "numri eshte me i vogel se 10";
+}
+
+?>
